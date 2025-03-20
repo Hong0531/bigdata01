e87adac11d3c40b80b55d09ece106e663995f3d8
hogogo7 <hogogo7@naver.com>
2025-03-20 11:54:08 +0900

    v0.5 prime number

--- week03_prime.py
+++ week03_prime.py
@@ -3,11 +3,13 @@ count = 0
 is_prime = True
 
 if number >= 2:
-    for i in range(2, number):
+    i = 2
+    while i*i <= number:
         if number % i == 0:
             count = count + 1
             is_prime = False
         print(i, end=" ")
+        i = i + 1
 else:
     count = 1
     is_prime = False
